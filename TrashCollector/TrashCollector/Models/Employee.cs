11713ff167fc13781438cc83e703f06f1601421f
Josi <JosiGrc@users.noreply.github.com>
2019-09-04 07:37:41 -0500

    Made both databases with required columms

---
+++ TrashCollector/TrashCollector/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        //Variables
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+
+        //ctor
+
+        //methods
+    }
+}
